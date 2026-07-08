b6857e8aad53cc7ec4fe13bc96f604afb04bc65a
ChessNeyshabur <chessneyshabur@example.com>
2026-07-08 15:36:14 +0430

    add player resume pages

---
+++ src/app/players/baran-shamsabadi/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>رزومه این بازیکن به‌زودی تکمیل می‌شود.</h1>
+    </main>
+  );
+}
