21e6d7cdf984900f9f6056b280f84e211279117e
ChessNeyshabur <chessneyshabur@example.com>
2026-07-08 16:13:27 +0430

    update project

---
+++ src/app/tournaments/rules/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+  return (
+    <main style={{ padding: "40px", textAlign: "center" }}>
+      <h1>قوانین مسابقات شطرنج</h1>
+      <p>این صفحه به‌زودی تکمیل می‌شود.</p>
+    </main>
+  );
+}
